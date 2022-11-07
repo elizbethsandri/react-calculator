46e2d593b7397e8c7b22f031cdeca3bc39ed2da7
Elizabeth Sandri <elizabethsandri18@gmail.com>
2022-11-07 22:11:47 +0000

    Instlled mui package + Started adding functions to buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import './Calculator.css';
+import Container from '@mui/material/Container';
+import { Box } from "@mui/system";
 
-export default function Calculator(){
-    return <div className="Wrapper">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>      
-      <button>5</button>     
-      <button>6</button>      
-      <button>7</button>    
-      <button>8</button>      
-      <button>9</button>
-      <button>0</button>      
-      <button>AC</button>
-      <button>+/-</button>
-      <button>%</button>
-      <button>/</button>
-      <button>*</button>
-      <button>-</button>
-      <button>+</button>
-      <button>=</button>
-      <button>,</button>
+export default function Calculator() {
+    const [number, setNumber] = useState(0);
+
+    function InputNumber(e) {
+        var input=e.target.value;
+        if (number === 0) {
+            setNumber(input);
+        } else {
+            setNumber(number + input);
+        }
+    }
+
+    function Clear() {
+        setNumber(0);
+    }
+
+    function Percentage() {
+        setNumber(number / 100);
+    }
+
+    function ChangeSign() {
+        if (number > 0) {
+            setNumber(-number);
+        } else {
+            setNumber(Math.abs(number));
+        }
+    }
+
+    return (
+    <div>
+    <Box m={12}/>
+    <Container maxWidth="xs"> 
+    <div className="Wrapper"> 
+      <h1 className="Result">{number}</h1>
+      <button onClick={Clear}>AC</button>
+      <button onClick={ChangeSign}>+/-</button>
+      <button onClick={Percentage}>%</button>
+      <button className="Orange">/</button>     
+      <button className="Gray" onClick={InputNumber} value={7}>7</button>  
+      <button className="Gray" onClick={InputNumber} value={8}>8</button>     
+      <button className="Gray" onClick={InputNumber} value={9}>9</button>
+      <button className="Orange">X</button>
+      <button className="Gray" onClick={InputNumber} value={4}>4</button>  
+      <button className="Gray" onClick={InputNumber} value={5}>5</button>       
+      <button className="Gray" onClick={InputNumber} value={6}>6</button>
+      <button className="Orange">-</button>   
+      <button className="Gray" onClick={InputNumber} value={1}>1</button>
+      <button className="Gray" onClick={InputNumber} value={2}>2</button>
+      <button className="Gray" onClick={InputNumber} value={3}>3</button> 
+      <button className="Orange">+</button>  
+      <button className="Gray" onClick={InputNumber} value={0}>0</button>
+      <button className="Gray" onClick={InputNumber} value={`,`}>,</button> 
+      <button className="Orange">=</button>
     </div>
+    </Container>
+    </div>
+    );
 }
