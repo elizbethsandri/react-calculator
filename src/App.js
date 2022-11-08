358ae8ab78301fe8a1fe3716f6b8ad0b17e7a108
Elizabeth Sandri <elizabethsandri18@gmail.com>
2022-11-08 14:02:07 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ function App() {
   return (
     <div className="App">
       <Calculator/>
+      <footer>
+          This project was coded by{" "}
+          <a
+            href="https://lizsandri-portfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Liz Sandri</a>,{" "}open-sourced on {" "}
+          <a
+            href="https://github.com/elizbethsandri/react-calculator"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub</a>{" "}
+          and{" "} hosted on {" "}
+          <a
+            href="https://react-calculator-bylizsandri.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             Netlify</a>
+        </footer>
     </div>
   );
 }
